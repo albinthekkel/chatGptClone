7e04421ad3a1e40c8e4a0e3d5727c9ec2ba5ac2d
albinthekkel <aswinalbin@gmail.com>
2024-06-20 19:30:33 +0100

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const cors = require("cors");
+const whitelist = [
+  "http://localhost:5000",
+  "http://localhost:3000",
+  "http://localhost:3001",
+];
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.post("/chat", async (req, res) => {
   const userInput = req.body.input;
